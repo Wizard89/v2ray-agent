f7f7e3f2613ecb38780dc30ed6190c131b3016eb
Wizard <66345978+Wizard89@users.noreply.github.com>
2023-05-16 17:38:55 +0800

    feat(脚本): update

--- install.sh
+++ install.sh
@@ -7099,7 +7099,7 @@ menu() {
 	echoContent red "\n=============================================================="
 	echoContent green "原作者：mack-a"
 	echoContent green "作者：Wizard89"
-	echoContent green "当前版本：v2.7.1"
+	echoContent green "当前版本：v2.7.2"
 	echoContent green "Github：https://github.com/Wizard89/v2ray-agent"
 	echoContent green "描述：八合一共存脚本\c"
 	showInstallStatus
