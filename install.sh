edc014bd70f6b920db2b56963db37259d27df978
Wizard <66345978+Wizard89@users.noreply.github.com>
2023-10-13 16:38:09 +0800

    fix(脚本): 修改错误符号

--- install.sh
+++ install.sh
@@ -2817,7 +2817,7 @@ initHysteriaPort() {
 initHysteriaProtocol() {
 	echoContent skyBlue "\n请选择协议类型"
 	echoContent red "=============================================================="
-	echoContent yellow "1.udp(QUIC)(默认)"
+    echoContent yellow "1.udp(QUIC)(默认)"
 	echoContent yellow "2.faketcp"
 	echoContent yellow "3.wechat-video"
 	echoContent red "=============================================================="
@@ -8176,7 +8176,7 @@ menu() {
 	echoContent red "\n=============================================================="
 	echoContent green "原作者：mack-a"
 	echoContent green "作者：Wizard89"
-	echoContent green "当前版本：v2.8.15"
+	echoContent green "当前版本：v2.8.16"
 	echoContent green "Github：https://github.com/Wizard89/v2ray-agent"
 	echoContent green "描述：八合一共存脚本\c"
 	showInstallStatus
