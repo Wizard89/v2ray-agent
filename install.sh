ea9d6d5f84ad5ba4440a7d9d09beaa7844bbbc50
Wizard <66345978+Wizard89@users.noreply.github.com>
2024-02-23 15:32:10 +0800

    fix(脚本): 修复无法下载sing-box内核问题

--- install.sh
+++ install.sh
@@ -2031,8 +2031,8 @@ installSingBox() {
     echoContent skyBlue "\n进度  $1/${totalProgress} : 安装sing-box"
 
     if [[ -z "${singBoxConfigPath}" ]]; then
-        #        version="v1.7.8"
-        version=$(curl -s "https://api.github.com/repos/SagerNet/sing-box/releases?per_page=10" | jq -r ".[]|select (.prerelease==${prereleaseStatus})|.tag_name" | head -1)
+
+        version=$(curl -s "https://api.github.com/repos/SagerNet/sing-box/releases?per_page=20" | jq -r ".[]|select (.prerelease==${prereleaseStatus})|.tag_name" | head -1)
 
         echoContent green " ---> sing-box版本:${version}"
 
@@ -8781,7 +8781,7 @@ menu() {
 	echoContent red "\n=============================================================="
 	echoContent green "原作者：mack-a"
 	echoContent green "作者：Wizard89"
-	echoContent green "当前版本：v3.0.9"
+	echoContent green "当前版本：v3.0.10"
 	echoContent green "Github：https://github.com/Wizard89/v2ray-agent"
 	echoContent green "描述：八合一共存脚本\c"
     showInstallStatus
