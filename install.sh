cec869e609d760079a8ea3804cc51081fa2032f1
Wizard <66345978+Wizard89@users.noreply.github.com>
2024-05-14 16:30:30 +0800

    fix(脚本): 修改异常报错问题

--- install.sh
+++ install.sh
@@ -606,14 +606,13 @@ allowPort() {
                 checkUFWAllowPort "$1"
             fi
         fi
-    elif rc-update show | grep ufw 2>/dev/null | grep -q "ufw"; then
+    elif rc-update show 2>/dev/null | grep -q ufw; then
         if ufw status | grep -q "Status: active"; then
             if ! ufw status | grep -q "$1/${type}"; then
                 sudo ufw allow "$1/${type}"
                 checkUFWAllowPort "$1"
             fi
         fi
-
     elif systemctl status firewalld 2>/dev/null | grep -q "active (running)"; then
         local updateFirewalldStatus=
         if ! firewall-cmd --list-ports --permanent | grep -qw "$1/${type}"; then
@@ -9114,7 +9113,7 @@ menu() {
 	echoContent red "\n=============================================================="
 	echoContent green "原作者：mack-a"
 	echoContent green "作者：Wizard89"
-	echoContent green "当前版本：v3.0.45"
+	echoContent green "当前版本：v3.0.46"
 	echoContent green "Github：https://github.com/Wizard89/v2ray-agent"
 	echoContent green "描述：八合一共存脚本\c"
     showInstallStatus
