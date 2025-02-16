e34559995f23838fe9b59dd3c3bae741e5141183
Wizard <66345978+Wizard89@users.noreply.github.com>
2025-02-16 12:39:57 +0800

    feat(脚本): 更新sing-box订阅配置以忽略警告

--- install.sh
+++ install.sh
@@ -9751,7 +9751,7 @@ menu() {
 	echoContent red "\n=============================================================="
 	echoContent green "原作者：mack-a"
 	echoContent green "作者：Wizard89"
-	echoContent green "当前版本：v3.1.29"
+	echoContent green "当前版本：v3.1.30"
 	echoContent green "Github：https://github.com/Wizard89/v2ray-agent"
 	echoContent green "描述：八合一共存脚本\c"
     showInstallStatus
