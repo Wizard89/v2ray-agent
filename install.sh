edf2a3cf5d5f633e3d10153db18d79bac7dc1d56
Wizard <66345978+Wizard89@users.noreply.github.com>
2025-09-02 00:07:24 +0800

    fix(脚本): 修复sing-box DNS Timeout问题

--- install.sh
+++ install.sh
@@ -9944,7 +9944,7 @@ menu() {
 	echoContent red "\n=============================================================="
 	echoContent green "原作者：mack-a"
 	echoContent green "作者：Wizard89"
-	echoContent green "当前版本：v3.2.21"
+	echoContent green "当前版本：v3.2.22"
 	echoContent green "Github：https://github.com/Wizard89/v2ray-agent"
 	echoContent green "描述：八合一共存脚本\c"
     showInstallStatus
